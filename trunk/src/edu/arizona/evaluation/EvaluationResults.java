53e4b038ab8951811e2bebf658702617fa1088f7
dhewlett <dhewlett@c4b787b0-236f-11df-bff0-d338b04bc40e>
2010-08-14 23:19:51 +0000

    Added some BVE+MDL code to main repo

--- trunk/src/edu/arizona/evaluation/EvaluationResults.java
+++ trunk/src/edu/arizona/evaluation/EvaluationResults.java
@@ -26,7 +26,7 @@ public class EvaluationResults {
    public void printResults() {
 //	   System.out.println("VE Number of Chunks:     " + estimatedChunkCount);
 //	   System.out.println("Actual Number of Chunks: " + actualChunkCount);
-	   System.out.println("BP\t\tBR\t\tBF\t\tWP\t\tWR\t\tWF");
+	   System.out.println("BP\tBR\tBF\tWP\tWR\tWF");
 	   System.out.print(NF.format(boundaryPrecision) + "\t" + NF.format(boundaryRecall) + "\t" + NF.format(boundaryF1()) + "\t");
 	   System.out.print(NF.format(chunkPrecision) + "\t" + NF.format(chunkRecall) + "\t" + NF.format(chunkF1()));
 	   System.out.println();
