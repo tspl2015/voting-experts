a24b70f96d71aa6341c227d2e6fa68cc5738477e
dhewlett <dhewlett@c4b787b0-236f-11df-bff0-d338b04bc40e>
2011-01-31 08:16:03 +0000

    Added more informative output for BVE+MDL, some simple statistics functions, and also added additional test corpora

--- trunk/src/edu/arizona/ve/demo/ManualBVEDemo.java
+++ trunk/src/edu/arizona/ve/demo/ManualBVEDemo.java
@@ -14,14 +14,21 @@ public class ManualBVEDemo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+//		Engine.DEBUG = true;
+//		Engine.EVALUATE = true;
+		
 		// Load the corpus 
 		Corpus corpus = Corpus.autoLoad("bloom73", CorpusType.LETTER, true);
+		
 		// Create a segmentation engine, with a trie depth of 6
-		Engine engine = new Engine(corpus, 6);
+		Engine engine = new Engine(corpus, 7);
+		
 		// Segment using BVE with manually-specified parameters
-		Segmentation segmentation = engine.voteBVE(5, 6, true, true);
+		Segmentation segmentation = engine.voteBVE(6, 0, true, true);
+		
 		// Evaluate (compute precision, recall, F-measure, etc.) 
 		EvaluationResults results = Evaluator.evaluate(segmentation, corpus);
+		
 		// Print the important evaluation metrics
 		results.printResults();
 	}
